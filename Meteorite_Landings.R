dc4745a2447521e8edc65d5e02cd786f7a3d96ca
Z-Lord79 <134428652+Z-Lord79@users.noreply.github.com>
2023-10-18 11:40:45 -0700

    add color changing bar graph

---
+++ Meteorite_Landings.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+
+data <- read_csv('Meteorite_Landings.csv')
+
+
+
+
+# rename mass(g)
+names(data)[names(data) == "mass (g)"] <- "mass"
+
+#use gg plot to make a bar graph based on year
+
+ggplot(data = data,
+       mapping = aes(x = year)) +
+  geom_bar()
+
+# use ggplot to create a bar graph based on name and mass
+
+ggplot(data = data,
+       mapping = aes(x = name,
+                     y = mass)) +
+  geom_bar(stat = "identity")
+
+
+# add color
+
+ggplot(data = data,
+       mapping = aes(x = name,
+                     y = mass,
+                     fill = INCTCFLG_A)) +
+  geom_col()
